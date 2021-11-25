56d11e044a9d1b95e365be91a10fc4e7bbc57ee1
Opilane TTHK <opilane@tthk.ee>
2021-11-25 09:59:39 +0200

    Week13-OOP

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace OOPPart1
+{
+    class Program
+    {
+        class dog
+        {
+            string name;
+            int spots;
+            double happiness;
+
+            public dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} with {spots} has been creaed!");
+            }
+            public string Name
+            {
+                get { return Name; }
+            }
+            public int Spots
+            {
+                get { return spots; }
+            }
+
+            public void Barks()
+            {
+                Console.WriteLine("Woof Woof");
+                happiness += 0.2;
+            }
+            public void Waggs()
+            {
+                Console.WriteLine("*Wiggle Wiggle*");
+            }
+            
+            public void rename(string newname)
+            {
+                name = newname;
+            }
+
+            static void Main(string[] args)
+            {
+                dog mydog = new dog("Jamal", 5);
+                // mydog.happiness = 10;
+                //mydog.name = "Jamal high as fuck";
+                while (mydog.happiness != 1)
+                {
+                    mydog.Barks();
+                }
+                Console.WriteLine("Rename you dog");
+                string userInput = Console.ReadLine();
+                mydog.rename(userInput);
+                Console.WriteLine($"{mydog.name}");
+                Console.WriteLine($"Level of happiness: {mydog.happiness}");
+
+            }
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        class cat
+        {
+            public string name;
+            public string color;
+            public double hungryness;
+
+            public cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hungryness = 0;
+                Console.WriteLine($"");
+            }
+            public string Name
+            {
+                get { return Name; }
+            }
+            public string Color
+            {
+                get { return color; }
+            }
+            public void Meows()
+            {
+                Console.WriteLine("Meow");
+                hungryness += 0.2;
+            }
+            public void Sleeps()
+            {
+                Console.WriteLine("*ZZZzzz*");
+            }
+
+            public void rename(string newname)
+            {
+                name = newname;
+            }
+            static void Main(string[] args)
+            {
+                cat mycat = new cat("Jamal", "purple");
+                
+                while (mycat.hungryness != 1)
+                {
+                    mycat.Meows();
+                }
+                Console.WriteLine("Rename you cat");
+                string userInput = Console.ReadLine();
+                mycat.rename(userInput);
+                Console.WriteLine($"{mycat.name}");
+                Console.WriteLine($"Level of hungryness: {mycat.hungryness}");
+            }
+        }
+    }
+}
